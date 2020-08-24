44581f76e9a91161eb604ce839f91588089ba09f
Ruzlan Karim <ruzlankarim2@gmail.com>
2020-08-25 02:33:26 +0600

    Updates

---
+++ precache-manifest.eaa6ece88b5878af5f727c0c0d4fdb22.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740b4b41f917f80ee804765e043febf3",
+    "url": "/react_jsonTOimage/index.html"
+  },
+  {
+    "revision": "4970a471de39a920a918",
+    "url": "/react_jsonTOimage/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "cb4d9155ef9ff898d9e1",
+    "url": "/react_jsonTOimage/static/css/main.8377b54e.chunk.css"
+  },
+  {
+    "revision": "4970a471de39a920a918",
+    "url": "/react_jsonTOimage/static/js/2.c62835da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_jsonTOimage/static/js/2.c62835da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb4d9155ef9ff898d9e1",
+    "url": "/react_jsonTOimage/static/js/main.8319f82c.chunk.js"
+  },
+  {
+    "revision": "2809d2990d5bb62c1abe",
+    "url": "/react_jsonTOimage/static/js/runtime-main.2cc7ba58.js"
+  },
+  {
+    "revision": "3d35049a875fbc4d3f3165da78f82ff4",
+    "url": "/react_jsonTOimage/static/media/HKGrotesk-Bold.3d35049a.otf"
+  },
+  {
+    "revision": "5ad7c6fb6cda7fb57b637fe6e0767593",
+    "url": "/react_jsonTOimage/static/media/HKGrotesk-Light.5ad7c6fb.otf"
+  },
+  {
+    "revision": "c471e9ace164521d3f8c2cd2461921fa",
+    "url": "/react_jsonTOimage/static/media/HKGrotesk-Medium.c471e9ac.otf"
+  },
+  {
+    "revision": "cab8839a909b408392b7b3147c2afd23",
+    "url": "/react_jsonTOimage/static/media/HKGrotesk-Regular.cab8839a.otf"
+  }
+]);
